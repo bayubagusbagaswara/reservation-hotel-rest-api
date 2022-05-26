7a0ee389b790337082c3ed8689e3272b3ccd17d2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-05-26 18:31:46 +0700

    initial commit

---
+++ src/main/java/com/bayu/reservation/ReservationRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/reservation/ReservationRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
