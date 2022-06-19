9138f81bb7827a3226fa8e16c58db88a99d48fca
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 09:56:52 +0700

    add enum RoleName

---
+++ src/main/java/com/bayu/reservation/entities/RoleName.java
@@ -0,0 +1,17 @@
+package com.bayu.reservation.entities;
+
+public enum RoleName {
+
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private final String roleName;
+
+    RoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
