e921343c59ae6c3d51b2e2c8cca0a634c2dfc805
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 18:42:04 +0700

    update exception NotFoundException

--- src/main/java/com/bayu/reservation/exception/NotFoundException.java
+++ src/main/java/com/bayu/reservation/exception/NotFoundException.java
@@ -1,28 +1,44 @@
 package com.bayu.reservation.exception;
 
+import com.bayu.reservation.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends Exception {
+public class NotFoundException extends RuntimeException {
 
-    public NotFoundException() {
+    private transient ApiResponse apiResponse;
+
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
+
+    public NotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super();
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
     }
 
-    public NotFoundException(String message) {
-        super(message);
+    public String getResourceName() {
+        return resourceName;
     }
 
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public String getFieldName() {
+        return fieldName;
     }
 
-    public NotFoundException(Throwable cause) {
-        super(cause);
+    public Object getFieldValue() {
+        return fieldValue;
     }
 
-    protected NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public ApiResponse getApiResponse() {
+        return apiResponse;
     }
+
+    private void setApiResponse() {
+        String message = String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue);
+        apiResponse = new ApiResponse(Boolean.FALSE, message);
+    }
+
 }
