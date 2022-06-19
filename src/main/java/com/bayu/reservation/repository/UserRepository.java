9b7fc5cc2c96a7234287548fab03c9de34299158
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 11:41:25 +0700

    add interface UserRepository

---
+++ src/main/java/com/bayu/reservation/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.bayu.reservation.repository;
+
+import com.bayu.reservation.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+    User findByUsername(String username);
+
+    List<User> findByRoles_Id(Long id);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
