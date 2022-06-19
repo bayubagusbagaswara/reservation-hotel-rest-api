0578993db93315e2e606651dc77b9088dad5ef5e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 12:38:35 +0700

    add contract service RoleService

---
+++ src/main/java/com/bayu/reservation/service/RoleService.java
@@ -0,0 +1,21 @@
+package com.bayu.reservation.service;
+
+import com.bayu.reservation.dto.RoleDTO;
+
+import java.util.List;
+
+public interface RoleService {
+
+    RoleDTO save(RoleDTO roleDTO);
+
+    List<RoleDTO> saveRoles(List<RoleDTO> roleDTOS);
+
+    List<RoleDTO> listAll();
+
+    RoleDTO getById(Long id);
+
+    RoleDTO getRoleByName(String name);
+
+    void deleteRole(Long id);
+
+}
