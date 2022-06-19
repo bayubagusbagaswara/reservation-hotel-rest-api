313fca9cedba85823097bac31cf9bd39a8ed4ce1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 11:48:02 +0700

    update RoleRepository

--- src/main/java/com/bayu/reservation/repository/RoleRepository.java
+++ src/main/java/com/bayu/reservation/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package com.bayu.reservation.repository;
 import com.bayu.reservation.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Role findByName(String roleName);
+    Optional<Role> findByName(String roleName);
 }
