e1b5b141f6a901a6b3d9a8cad6ed446a28c71f79
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 18:18:28 +0700

    add class WebResponse

---
+++ src/main/java/com/bayu/reservation/dto/WebResponse.java
@@ -0,0 +1,45 @@
+package com.bayu.reservation.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder@JsonPropertyOrder({
+        "success",
+        "message",
+        "data"
+})
+@AllArgsConstructor
+@NoArgsConstructor
+public class WebResponse<T> {
+
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("data")
+    private T data;
+
+    @JsonIgnore
+    private HttpStatus status;
+
+    public WebResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public WebResponse(Boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
+
