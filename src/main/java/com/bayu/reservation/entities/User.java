8e50ac2230caf6fec910c4826e601e7c8139ea9d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 09:57:05 +0700

    add entity User

---
+++ src/main/java/com/bayu/reservation/entities/User.java
@@ -0,0 +1,69 @@
+package com.bayu.reservation.entities;
+
+import com.bayu.reservation.entities.audit.DateAudit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends DateAudit implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String firstName;
+
+    @Column(nullable = false, length = 40)
+    private String lastName;
+
+    private String username;
+
+    @Column(unique = true, length = 40, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean active;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<Booking> bookings = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "id_user", foreignKey = @ForeignKey(name = "fk_user_role_id_user"), referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_role", foreignKey = @ForeignKey(name = "fk_user_role_id_role"), referencedColumnName = "id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        this.getRoles().remove(role);
+    }
+
+    public void removeRoles() {
+        for (Role role : new HashSet<>(roles)) {
+            removeRole(role);
+        }
+    }
+
+}
