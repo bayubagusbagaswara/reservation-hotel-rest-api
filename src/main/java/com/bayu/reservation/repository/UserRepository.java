d793729ae065d493e9f0b7915c1619329e155dd3
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 11:47:48 +0700

    update UserRepository

--- src/main/java/com/bayu/reservation/repository/UserRepository.java
+++ src/main/java/com/bayu/reservation/repository/UserRepository.java
@@ -4,12 +4,13 @@ import com.bayu.reservation.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     List<User> findByRoles_Id(Long id);
 
