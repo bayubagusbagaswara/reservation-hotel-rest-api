d287ad3e115fac71e1a2ab5720692aa00b6e4298
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 18:18:11 +0700

    add class ApiResponse

---
+++ src/main/java/com/bayu/reservation/dto/ApiResponse.java
@@ -0,0 +1,40 @@
+package com.bayu.reservation.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@JsonPropertyOrder({
+        "success",
+        "message"
+})
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonIgnore
+    private HttpStatus status;
+
+    public ApiResponse(Boolean success) {
+        this.success = success;
+    }
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+}
