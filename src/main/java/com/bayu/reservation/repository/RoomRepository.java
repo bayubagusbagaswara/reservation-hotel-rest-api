53f5da88d35eae127ffe75ecb02b3673906511e8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-19 11:53:56 +0700

    add interface RoomRepository

---
+++ src/main/java/com/bayu/reservation/repository/RoomRepository.java
@@ -0,0 +1,28 @@
+package com.bayu.reservation.repository;
+
+import com.bayu.reservation.entities.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    Room findByName(String name);
+
+    @Query(value = "select * from room as ro "
+            + "where ro.id not in "
+            + "("
+            + "select re.room_id "
+            + "from booking as re "
+            + "where (re.start_date BETWEEN ?1 and ?2) "
+            + "or (re.end_date BETWEEN ?1 and  ?2) "
+            + ")", nativeQuery = true)
+    List<Room> findMeetingRoomAvailable(LocalDateTime db, LocalDateTime de);
+
+    @Query(value = "select * from (select * from room as ro where ro.id not in "
+            + "(select re.room_id from booking as re where (re.start_date BETWEEN ?1 and  ?2) "
+            + "or (re.end_date BETWEEN ?1 and ?2))) as roo where roo.id = ?3", nativeQuery = true)
+    Room checkAvailability(LocalDateTime db, LocalDateTime de, Long id);
+}
