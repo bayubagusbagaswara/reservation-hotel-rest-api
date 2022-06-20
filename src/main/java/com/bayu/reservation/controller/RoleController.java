c0037d035245c4d92343172873d787ce1042b525
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-20 20:26:25 +0700

    add api RoleController

---
+++ src/main/java/com/bayu/reservation/controller/RoleController.java
@@ -0,0 +1,22 @@
+package com.bayu.reservation.controller;
+
+import com.bayu.reservation.service.RoleService;
+import com.bayu.reservation.service.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(value = "/api/roles")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    private final UserService userService;
+
+    public RoleController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+}
