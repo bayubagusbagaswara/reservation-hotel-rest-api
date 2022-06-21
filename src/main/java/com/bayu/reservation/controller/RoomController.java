e37f81ac1fd777f071e6019dc7f8f7088266fb96
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-21 18:28:39 +0700

    add method getAllRooms in RoomController

---
+++ src/main/java/com/bayu/reservation/controller/RoomController.java
@@ -0,0 +1,32 @@
+package com.bayu.reservation.controller;
+
+import com.bayu.reservation.dto.RoomDTO;
+import com.bayu.reservation.service.RoomService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(value = "/api/rooms")
+public class RoomController {
+
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoomDTO>> getAllRooms() {
+        List<RoomDTO> roomDTOS = roomService.listAll();
+        return new ResponseEntity<>(roomDTOS, HttpStatus.OK);
+    }
+
+
+}
