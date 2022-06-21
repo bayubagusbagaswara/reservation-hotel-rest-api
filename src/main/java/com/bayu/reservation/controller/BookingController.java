e61fed3fb5bbb5e15e4df522c291e755265817fb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-21 19:39:23 +0700

    add method getBookingById in BookingController

--- src/main/java/com/bayu/reservation/controller/BookingController.java
+++ src/main/java/com/bayu/reservation/controller/BookingController.java
@@ -5,10 +5,7 @@ import com.bayu.reservation.service.BookingService;
 import com.bayu.reservation.service.EmailSenderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,17 +16,20 @@ public class BookingController {
 
     private final BookingService bookingService;
 
-    private final EmailSenderService emailSenderService;
-
-    public BookingController(BookingService bookingService, EmailSenderService emailSenderService) {
+    public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
-        this.emailSenderService = emailSenderService;
     }
 
     @GetMapping
     public ResponseEntity<List<BookingDTO>> getAllBookings() {
         List<BookingDTO> bookingDTOS = bookingService.listAll();
         return new ResponseEntity<>(bookingDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<BookingDTO> getBookingById(@PathVariable(name = "id") Long bookingId) {
+        BookingDTO bookingDTO = bookingService.getById(bookingId);
+        return new ResponseEntity<>(bookingDTO, HttpStatus.OK);
     }
 
 }
