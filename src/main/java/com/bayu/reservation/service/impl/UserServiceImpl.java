fa5239081f1d4c6ab60e70d501331757fabf0ce1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 19:17:40 +0700

    removing method loadUserByUsername

--- src/main/java/com/bayu/reservation/service/impl/UserServiceImpl.java
+++ src/main/java/com/bayu/reservation/service/impl/UserServiceImpl.java
@@ -10,19 +10,13 @@ import com.bayu.reservation.repository.RoleRepository;
 import com.bayu.reservation.repository.UserRepository;
 import com.bayu.reservation.security.SecurityParams;
 import com.bayu.reservation.service.UserService;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -148,13 +142,4 @@ public class UserServiceImpl implements UserService {
         return userConvert.entityToDto(user);
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new NotFoundException("User", "username", username));
-        if (user == null) {
-            throw new UsernameNotFoundException("Username not found in database");
-        }
-        Collection<GrantedAuthority> authorities = user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getRoleName().name())).collect(Collectors.toList());
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
-    }
 }
