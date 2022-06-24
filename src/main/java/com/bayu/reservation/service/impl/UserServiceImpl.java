59a96e4e65c9694245a59c17c3e762c99fa888c2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-24 19:13:25 +0700

    add method getUserByBookingId and loadUserByUsername

--- src/main/java/com/bayu/reservation/service/impl/UserServiceImpl.java
+++ src/main/java/com/bayu/reservation/service/impl/UserServiceImpl.java
@@ -1,53 +1,43 @@
 package com.bayu.reservation.service.impl;
 
 import com.bayu.reservation.dto.ApiResponse;
-import com.bayu.reservation.dto.BookingDTO;
 import com.bayu.reservation.dto.UserDTO;
 import com.bayu.reservation.entities.Role;
 import com.bayu.reservation.entities.User;
 import com.bayu.reservation.exception.NotFoundException;
-import com.bayu.reservation.mapper.BookingConvert;
 import com.bayu.reservation.mapper.UserConvert;
-import com.bayu.reservation.repository.BookingRepository;
 import com.bayu.reservation.repository.RoleRepository;
 import com.bayu.reservation.repository.UserRepository;
 import com.bayu.reservation.security.SecurityParams;
-import com.bayu.reservation.service.BookingService;
-import com.bayu.reservation.service.EmailSenderService;
 import com.bayu.reservation.service.UserService;
-import com.bayu.reservation.util.Form;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final BookingRepository bookingRepository;
     private final RoleRepository roleRepository;
-    private final BookingService bookingService;
     private final UserConvert userConvert;
-    private final BookingConvert bookingConvert;
     private final PasswordEncoder passwordEncoder;
-    private final EmailSenderService emailSenderService;
 
-    public UserServiceImpl(UserRepository userRepository, BookingRepository bookingRepository, RoleRepository roleRepository, BookingService bookingService, UserConvert userConvert, BookingConvert bookingConvert, PasswordEncoder passwordEncoder, EmailSenderService emailSenderService) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, UserConvert userConvert, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.bookingRepository = bookingRepository;
         this.roleRepository = roleRepository;
-        this.bookingService = bookingService;
         this.userConvert = userConvert;
-        this.bookingConvert = bookingConvert;
         this.passwordEncoder = passwordEncoder;
-        this.emailSenderService = emailSenderService;
     }
 
     @Override
@@ -153,27 +143,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public String bookRoom(Form.UserBookingForm form, Long id) {
-        return null;
-    }
-
-    @Override
-    public String cancelBooking(Long id, String Code) {
-        return null;
-    }
-
-    @Override
-    public List<BookingDTO> getReservations(Long id) {
-        return null;
-    }
-
-    @Override
-    public List<BookingDTO> getBookingsByUserId(Long id) {
-        return null;
+    public UserDTO getUserByBookingId(Long bookingId) {
+        User user = userRepository.findByBookingsId(bookingId).orElseThrow(() -> new NotFoundException("User", "bookingId", bookingId));
+        return userConvert.entityToDto(user);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new NotFoundException("User", "username", username));
+        if (user == null) {
+            throw new UsernameNotFoundException("Username not found in database");
+        }
+        Collection<GrantedAuthority> authorities = user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getRoleName().name())).collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 }
