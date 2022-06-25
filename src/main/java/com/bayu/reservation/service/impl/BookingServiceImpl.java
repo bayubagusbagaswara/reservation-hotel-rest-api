4ff48e7f3e85d50bda7fe75bd37fe15f00941bd8
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-25 14:35:28 +0300

    add logic of method bookRoom

--- src/main/java/com/bayu/reservation/service/impl/BookingServiceImpl.java
+++ src/main/java/com/bayu/reservation/service/impl/BookingServiceImpl.java
@@ -1,20 +1,29 @@
 package com.bayu.reservation.service.impl;
 
 import com.bayu.reservation.dto.BookingDTO;
+import com.bayu.reservation.dto.UserDTO;
 import com.bayu.reservation.entities.Booking;
 import com.bayu.reservation.entities.Room;
+import com.bayu.reservation.entities.User;
 import com.bayu.reservation.exception.NotFoundException;
 import com.bayu.reservation.mapper.BookingConvert;
+import com.bayu.reservation.mapper.UserConvert;
 import com.bayu.reservation.repository.BookingRepository;
 import com.bayu.reservation.repository.RoomRepository;
+import com.bayu.reservation.repository.UserRepository;
+import com.bayu.reservation.security.SecurityParams;
 import com.bayu.reservation.service.BookingService;
+import com.bayu.reservation.service.EmailSenderService;
+import com.bayu.reservation.service.UserService;
 import com.bayu.reservation.util.Form;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Collection;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -26,13 +35,22 @@ public class BookingServiceImpl implements BookingService {
 //    private final static int MIN_VALUE = 100000000;
 
     private final BookingRepository bookingRepository;
+    private final UserRepository userRepository;
     private final BookingConvert bookingConvert;
     private final RoomRepository roomRepository;
+    private final UserConvert userConvert;
+    private final UserService userService;
 
-    public BookingServiceImpl(BookingRepository bookingRepository, BookingConvert bookingConvert, RoomRepository roomRepository) {
+    private final EmailSenderService emailSenderService;
+
+    public BookingServiceImpl(BookingRepository bookingRepository, UserRepository userRepository, BookingConvert bookingConvert, RoomRepository roomRepository, UserConvert userConvert, UserService userService, EmailSenderService emailSenderService) {
         this.bookingRepository = bookingRepository;
+        this.userRepository = userRepository;
         this.bookingConvert = bookingConvert;
         this.roomRepository = roomRepository;
+        this.userConvert = userConvert;
+        this.userService = userService;
+        this.emailSenderService = emailSenderService;
     }
 
     @Override
@@ -99,7 +117,47 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public String bookRoom(Form.UserBookingForm form, Long id) {
-        return null;
+        LocalDateTime startDate = LocalDateTime.parse(form.getDateStart());
+        LocalDateTime endDate = LocalDateTime.parse(form.getDateEnd());
+        UserDTO userDTO = userConvert.entityToDto(userRepository.findById(id).orElseThrow(() -> new NotFoundException("User", "id", id)));
+
+        BookingDTO booking = bookRoom(form.getRoomName(), startDate, endDate);
+        booking.setUser(userDTO);
+        booking.setDescription(form.getDescription());
+        if (startDate.isAfter(endDate) || endDate.isEqual(startDate) || endDate.isBefore(LocalDateTime.now()) || startDate.isBefore(LocalDateTime.now())) {
+            return "Your date does not fit criteria!";
+        }
+        BookingDTO bookingDTO = save(booking);
+
+        if (!Objects.isNull(bookingDTO)) {
+
+            Period period = Period.between(startDate.toLocalDate(), endDate.toLocalDate());
+            period = period.minusDays(endDate.toLocalTime().compareTo(startDate.toLocalTime()) >= 0 ? 0 : 1);
+            Duration duration = Duration.between(startDate, endDate);
+            duration = duration.minusDays(duration.toDays());
+            int hours = (int) duration.toHours();
+            String minutes = (duration.toMinutes() > 0 ? (duration.toMinutes() + " minutes") : "");
+            String days = period.getDays() > 1 ? " days" : " day";
+            String isday = (period.getDays() > 0 ? (period.getDays() + days) : "");
+            List<User> users = userConvert.dtoToEntity(userService.fetchUsersByRole(SecurityParams.ADMIN));
+            String Body = "Reservation " + booking.getCode() + ": User " + booking.getUser().getFirstName() + " has reserved room "
+                    + booking.getRoom().getName() + " with a duration of "
+                    + isday
+                    + hours + " Hour" + (hours > 1 ? "s " : " ")
+                    + minutes
+                    + "\n\nReservation starts at  " + startDate.format(DateTimeFormatter.ofPattern("EEE, d MMM yyyy HH:mm")) +
+                    " Please confirm  booking ID : " + bookingDTO.getId() +
+                    " with a  POST request at  http://localhost:8080/" +
+                    bookingDTO.getId() + "/confirm?confirmed=true";
+            for (User user : users) {
+                try {
+                    emailSenderService.sendEmail(user.getEmail(), Body, "Confirm Booking!");
+                } catch (MailException mailException) {
+                    mailException.printStackTrace();
+                }
+            }
+        }
+        return "Added Successfully";
     }
 
     @Override
