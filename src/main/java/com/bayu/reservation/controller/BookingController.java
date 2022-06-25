3ac42d418ed4e0ca9c3b03ca838a70d689bd5eda
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-25 16:19:52 +0700

    deactivated method

--- src/main/java/com/bayu/reservation/controller/BookingController.java
+++ src/main/java/com/bayu/reservation/controller/BookingController.java
@@ -29,11 +29,11 @@ public class BookingController {
         return new ResponseEntity<>(bookingDTOS, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<BookingDTO> getBookingById(@PathVariable(name = "id") Long bookingId) {
-        BookingDTO bookingDTO = bookingService.getById(bookingId);
-        return new ResponseEntity<>(bookingDTO, HttpStatus.OK);
-    }
+//    @GetMapping(value = "/{id}")
+//    public ResponseEntity<BookingDTO> getBookingById(@PathVariable(name = "id") Long bookingId) {
+//        BookingDTO bookingDTO = bookingService.getById(bookingId);
+//        return new ResponseEntity<>(bookingDTO, HttpStatus.OK);
+//    }
 
 //    @GetMapping(value = "/{id}/user")
 //    public ResponseEntity<UserDTO> getUserByBookingId(@PathVariable(name = "id") Long bookingId) {
