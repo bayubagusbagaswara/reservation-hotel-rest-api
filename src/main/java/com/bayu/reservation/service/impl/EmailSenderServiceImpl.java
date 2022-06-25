4ab8fbcc0056492e779c4f575cc1def3bcf5fc2f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-25 14:23:37 +0700

    implement logic method sendEmail

--- src/main/java/com/bayu/reservation/service/impl/EmailSenderServiceImpl.java
+++ src/main/java/com/bayu/reservation/service/impl/EmailSenderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.bayu.reservation.service.impl;
 
 import com.bayu.reservation.service.EmailSenderService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,8 +10,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class EmailSenderServiceImpl implements EmailSenderService {
 
+    private final JavaMailSender javaMailSender;
+
+    public EmailSenderServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
     @Override
     public void sendEmail(String to, String body, String subject) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
 
+        javaMailSender.send(message);
     }
 }
