a1b67bc6224fc8aeb226e3e7f396ef4ae934af1e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-26 09:55:19 +0700

    add implementation service ScheduleServiceImpl

---
+++ src/main/java/com/bayu/reservation/service/impl/ScheduleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bayu.reservation.service.impl;
+
+import com.bayu.reservation.service.ScheduleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ScheduleServiceImpl implements ScheduleService {
+
+    @Override
+    public void job1() {
+
+    }
+
+    @Override
+    public void job2() {
+
+    }
+}
